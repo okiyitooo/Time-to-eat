1e9e6b93401bb860f15c7ebec570ed44ae96fa15
kanaetochi okiyi <65520324+okiyitooo@users.noreply.github.com>
2022-02-03 22:54:44 -0500

    Made foundation

---
+++ ProjectStation/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+//we'll implement this
+#include <Player.h>
+#include <Stage.h>
+
+bool playerCanPlay = true;
+
+int main() {
+	//getPlayer name
+	//Player player{*name*};
+	while (playerCanPlay) {
+		//ask player what they want to eat
+		//give  options from fridge
+		//ask Player what they want to do
+		//give list of activities
+		//Time passes
+		std::cout << "Infinite loop" << std::endl
+		std::cin >> std::string(" ");
+	}
+	std::cout << "The end" <<std::endl;
+	return 0;
+}
+
